c43a7992f7cb2a48310856e48521314e4cd5c3a9
Diego Santos <diegoeustaquiodossantos@hotmail.com>
2022-12-14 08:52:45 -0300

    Created and configured front-web

---
+++ front-web/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div
+      style={{
+        width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'
+      }}
+    >
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
